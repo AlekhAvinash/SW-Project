2e7ab063bdd9aff73c52ec070b9406c3b2d06acb
AlekhAvinash <mcdanroy@gmail.com>
2022-01-20 15:29:27 +0530

    init-update

---
+++ app/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template, request
+from timeloop import Timeloop as tl
+
+@tl.job(interval=timedelta(seconds=1))
+def checker():
+
+@tl.job(interval=timedelta(seconds=1))
+def function2():
+
+if __name__ == '__main__':
+    
+    tl.start(block=True)
+
+app = Flask(__name__, template_folder = 'template')
+
+def add(url):
+	with open('URLs.txt', 'a') as f:
+		f.write(url+'\n')
+
+@app.route('/', methods = ["GET", "POST"])
+def home():
+	if request.method == "POST":
+		url = request.form.get('url')
+		add(url)
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(host="localhost", port=6080)
